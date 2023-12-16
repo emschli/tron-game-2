efae8904f79c90ed10098c42bf9f24c3fea17a5e
olgan <olga.nesterova@haw-hamburg.de>
2023-12-16 17:42:16 +0100

    added some comments for controller interfaces

--- src/main/java/haw/vs/controller/api/IInput.java
+++ src/main/java/haw/vs/controller/api/IInput.java
@@ -4,7 +4,12 @@ import haw.vs.common.Direction;
 import haw.vs.common.PlayerConfigData;
 
 public interface IInput {
+    // Ein Spieler tritt dem Spiel bei
     void joinGame(long playerId, int noOfPlayers, PlayerConfigData configData);
+
+    // Ein Spieler bricht das Warten ab
     void cancelWait(long playerId);
+
+    // Verarbeitet die Spielaktion eines Spielers
     void handleGameAction(long playerId, Direction dir);
 }
