1700400e7eb1e47f2198963f26797774142a6c12
olgan <olga.nesterova@haw-hamburg.de>
2023-12-17 15:32:09 +0100

    add basic impl for controller interfaces

---
+++ src/main/java/haw/vs/controller/impl/InputHandler.java
@@ -0,0 +1,29 @@
+package haw.vs.controller.impl;
+
+
+import haw.vs.common.Direction;
+import haw.vs.common.PlayerConfigData;
+import haw.vs.controller.api.IInput;
+
+public class InputHandler implements IInput {
+    private final IMatchController matchController;
+
+    public InputHandler(IMatchController matchController) {
+        this.matchController = matchController;
+    }
+
+    @Override
+    public void joinGame(long playerId, int noOfPlayers, PlayerConfigData configData) {
+        matchController.addPlayerToMatch(playerId, noOfPlayers, configData);
+    }
+
+    @Override
+    public void cancelWait(long playerId) {
+        matchController.deletePlayerFromMatch(playerId, matchId, noOfPlayers );
+    }
+
+    @Override
+    public void handleGameAction(long playerId, Direction dir) {
+        matchController.movePlayer(playerId, matchId, dir);
+    }
+}
