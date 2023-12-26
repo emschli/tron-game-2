0b8efda74a2014110cdb8e0ce0b205e379538a9c
Rebecca <rebecca.kassaye@haw-hamburg.de>
2023-12-26 10:51:41 +0100

    finish manageCollisions, add some small helper methods

--- src/main/java/haw/vs/model/gamelogic/impl/GameLogic.java
+++ src/main/java/haw/vs/model/gamelogic/impl/GameLogic.java
@@ -4,11 +4,12 @@ import haw.vs.common.Coordinate;
 import haw.vs.common.Direction;
 import haw.vs.model.common.Match;
 import haw.vs.model.common.Player;
+import haw.vs.model.common.PlayerState;
 import haw.vs.model.gamelogic.IGameLogic;
 import haw.vs.model.gamelogic.IGameStateProcessedHandler;
 import haw.vs.model.gamelogic.api.GameLogicFactory;
 
-import java.util.List;
+import java.util.*;
 
 public class GameLogic implements IGameLogic {
 
@@ -24,8 +25,7 @@ public class GameLogic implements IGameLogic {
     public void processMatch(Match match) {
         // process Game
         movePlayers(match);
-        checkCollisions(match);
-        //TODO killPlayers();
+        manageCollisions(match);
         //return to matchManager
         gameStateProcessedHandler.updateMatch(match);
     }
@@ -53,12 +53,12 @@ public class GameLogic implements IGameLogic {
     /**
      * Calculates the next coordinate, depending on current head and direction
      *
-     * @param goingTo - the direction a player is currently driving towards
-     * @param head    - the coordinate at thr front of the trace
+     * @param going - the direction a player is currently driving towards
+     * @param head  - the coordinate at the front of the trace
      * @return the new head
      */
-    private Coordinate getNextCoordinate(Direction goingTo, Coordinate head) {
-        return switch (goingTo) {
+    private Coordinate getNextCoordinate(Direction going, Coordinate head) {
+        return switch (going) {
             case UP -> head.add(new Coordinate(0, 1)); // y++
             case DOWN -> head.add(new Coordinate(0, -1)); // y--
             case LEFT -> head.add(new Coordinate(-1, 0)); // x--
@@ -69,21 +69,100 @@ public class GameLogic implements IGameLogic {
 
     /**
      * Checks if any of the players alive are collided with borders,
-     * traces (their own or others) or had a head-on collision
+     * traces (their own or others) or had a head-on collision and kills them.
+     *
      * @param match - the current match containing the players in question
      */
     //TODO entweder liste zurückgeben oder zum schluss kollisionen löschen
-    private void checkCollisions(Match match) {
-        // check if within boundaries
-        List<Player> toKill = match.getAlivePlayers().stream()
-                .filter(player -> !inBoundaries(player.getHead(), match.getMaxGridX(), match.getMaxGridY()))
-                .toList();
-        // check for collision with other players head-on
-        // check for collision with other players trace
+//    private void manageCollisions(Match match) {
+//        List<Player> toKill = new ArrayList<>();
+//        Map<Coordinate, Player> headMap = new HashMap<>();
+//
+//        for (Player player : match.getAlivePlayers()) {
+//            // check if within boundaries
+//            if (!inBoundaries(player.getHead(), match.getMaxGridX(), match.getMaxGridY())) {
+//                toKill.add(player);
+//            }
+//            // check for collision with other players head-on (bc both have to be killed)
+//            if (headMap.containsKey(player.getHead())) {
+//                toKill.add(player);
+//                toKill.add(headMap.get(player.getHead()));
+//            } else {
+//                headMap.put(player.getHead(), player);
+//            }
+//            // check for collision with traces of other players
+//            for (Player otherPlayer : match.getAlivePlayers()) {
+//                if (otherPlayer != player && isColliding(player.getHead(), otherPlayer.getTrace())) {
+//                    toKill.add(player);
+//                    break;
+//                }
+//            }
+//            // check for collision of own trace
+//            if (isCollidingWithOwnTrace(player, player.getTrace())) {
+//                toKill.add(player);
+//            }
+//        }
+//        killPlayers(toKill);
+//    }
+    private void manageCollisions(Match match) {
+        Set<Player> toKill = new HashSet<>();
+        Map<Coordinate, Player> headMap = new HashMap<>();
 
+        for (Player player : match.getAlivePlayers()) {
+            Coordinate head = player.getHead();
+
+            if (!inBoundaries(head, match.getMaxGridX(), match.getMaxGridY()) ||
+                    headMap.containsKey(head) ||
+                    match.getAlivePlayers().stream().anyMatch(otherPlayer ->
+                            otherPlayer != player &&
+                                    (isColliding(head, otherPlayer.getTrace()) || isCollidingWithOwnTrace(player)))) {
+                toKill.add(player);
+            } else {
+                headMap.put(head, player);
+            }
+        }
+        killPlayers(toKill);
     }
 
+    /**
+     * Sets the player state of a given set of players to dead
+     *
+     * @param toKill - the set of players to be killed
+     */
+    private void killPlayers(Set<Player> toKill) {
+        for (Player p : toKill) {
+            p.setState(PlayerState.DEAD);
+        }
+    }
 
+    /**
+     * Checks if a player is colliding with their own trace.
+     *
+     * @param player - player to be checked
+     * @return true, if player is collided with own trace, false else
+     */
+    private boolean isCollidingWithOwnTrace(Player player) {
+        return player.getTrace().subList(0, player.getTrace().size() - 1).contains(player.getHead());
+    }
+
+    /**
+     * Checks if the head is colliding with a given trace
+     *
+     * @param head  - last element of the player
+     * @param trace - the trace of another player in the match
+     * @return true if collision took place, false else
+     */
+    private boolean isColliding(Coordinate head, List<Coordinate> trace) {
+        return trace.contains(head);
+    }
+
+    /**
+     * Checks, if player is driving out of the gridbounds.
+     * @param newHead - the coordinate added to the trace last
+     * @param maxX
+     * @param maxY
+     * @return true, if player is driving within the boundaries, false else.
+     */
     private boolean inBoundaries(Coordinate newHead, int maxX, int maxY) {
         return newHead.x >= 0 && newHead.x <= maxX && newHead.y >= 0 && newHead.y <= maxY;
     }
