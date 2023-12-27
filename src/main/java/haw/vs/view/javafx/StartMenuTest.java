858dd9aafac694b3d834424c000f4c68fe7ae399
Natalie Kratzsch <nataliekratzsch@gmx.de>
2023-12-27 13:58:42 +0100

    added javaFX example

---
+++ src/main/java/haw/vs/view/javafx/StartMenuTest.java
@@ -0,0 +1,32 @@
+package haw.vs.view.javafx;
+
+import edu.cads.bai5.vsp.tron.view.ITronView;
+import edu.cads.bai5.vsp.tron.view.ViewUtility;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+
+public class StartMenuTest extends VBox {
+    private final Label labelReady;
+    private final Button btnStart;
+
+    public StartMenuTest(String stylesheet, ITronView view) {
+        super(20.0);
+        this.getStylesheets().add(stylesheet);
+        this.setAlignment(Pos.CENTER);
+
+        labelReady = new Label("Ready?");
+        labelReady.setStyle("-fx-text-fill: " + ViewUtility.getHexTriplet(Color.PAPAYAWHIP.brighter()) + ";");
+
+        btnStart = new Button("Start Game");
+        btnStart.setOnAction(event -> {
+            System.out.println("click!");
+            view.hideOverlays();
+        });
+
+        this.getChildren().add(labelReady);
+        this.getChildren().add(btnStart);
+    }
+}
