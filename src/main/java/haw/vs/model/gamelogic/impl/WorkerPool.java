267eecb9f0407b3f7483888dcb193ea96c9f31fa
Rebecca <rebecca.kassaye@haw-hamburg.de>
2023-12-28 15:05:35 +0100

    cleanup WorkerPool, transfer of processed game handled by GameLogic

--- src/main/java/haw/vs/model/gamelogic/impl/WorkerPool.java
+++ src/main/java/haw/vs/model/gamelogic/impl/WorkerPool.java
@@ -2,27 +2,18 @@ package haw.vs.model.gamelogic.impl;
 
 import haw.vs.model.common.Match;
 import haw.vs.model.gamelogic.IWorkerPool;
-import haw.vs.model.gamelogic.IGameStateProcessedHandler;
-import haw.vs.model.gamelogic.api.GameLogicFactory;
 
 import java.util.concurrent.*;
 
 public class WorkerPool implements IWorkerPool {
 
-    private IGameStateProcessedHandler gameStateProcessedHandler;
-
     private ArrayBlockingQueue<Runnable> queue;
-
     private ThreadPoolExecutor threadPoolExecutor;
-
     private GameLogic gameLogic;
 
-    private ScheduledExecutorService scheduledExecutor;
 
     public WorkerPool() {
-        GameLogicFactory glf = new GameLogicFactory();
         this.gameLogic = new GameLogic();
-        this.gameStateProcessedHandler = glf.getGameStateProcessedHandler();
         this.queue = new ArrayBlockingQueue<Runnable>(5);
         this.threadPoolExecutor = new ThreadPoolExecutor(
                 3,
@@ -32,23 +23,17 @@ public class WorkerPool implements IWorkerPool {
                 queue);
         threadPoolExecutor.prestartAllCoreThreads();
 
-        // ScheduledExecutorService to check for inactivity and shutdown if no tasks added
-        this.scheduledExecutor = Executors.newScheduledThreadPool(1);
-        scheduledExecutor.scheduleWithFixedDelay(() -> {
-            if (queue.isEmpty()) {
-                System.out.println("No tasks added for 15 seconds. Shutting down executor...");
-                shutdownExecutor();
-            }
-        }, 15, 15, TimeUnit.SECONDS);
-
     }
 
+    /**
+     * Adds a match to a queue. Items in the queue are processed to the GameLocic.
+     * @param match
+     */
     @Override
     public void addTask(Match match) {
         // wrap match in a runnable
         Runnable task = () -> {
             gameLogic.processMatch(match);
-            //gameStateProcessedHandler.updateMatch(match);
         };
         // add runnable to Queue
         try {
@@ -58,42 +43,36 @@ public class WorkerPool implements IWorkerPool {
         }
     }
 
-    private void shutdownExecutor() {
-        threadPoolExecutor.shutdown();
-        scheduledExecutor.shutdown();
-    }
-
-    public static void main(String[] args) {
-        WorkerPool wp = new WorkerPool();
-
-
-        // Thread that adds tasks to the WorkerPool
-        Thread taskAdderThread = new Thread(() -> {
-            // Simulate adding tasks to the WorkerPool at intervals
-            for (int i = 1; i <= 10; i++) {
-                Match match = new Match(); // Replace with your Match constructor
-
-                wp.addTask(match);
-                System.out.println("Added Match_" + i + " to the worker pool.");
-
-                // Sleep for a short duration before adding the next task (for demonstration purposes)
-                try {
-                    Thread.sleep(2000); // Sleep for 2 seconds
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                }
-            }
-
-            // Sleep for 15 seconds after adding all tasks to simulate no tasks being added
-            try {
-                Thread.sleep(15000); // Sleep for 15 seconds (no tasks added)
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-        });
-
-        taskAdderThread.start();
-    }
+//    public static void main(String[] args) {
+//        WorkerPool wp = new WorkerPool();
+//
+//        // Thread that adds tasks to the WorkerPool
+//        Thread taskAdderThread = new Thread(() -> {
+//            // Simulate adding tasks to the WorkerPool at intervals
+//            for (int i = 1; i <= 10; i++) {
+//                Match match = new Match(i); // Replace with your Match constructor
+//
+//                wp.addTask(match);
+//                System.out.println("Added Match_" + i + " to the worker pool.");
+//
+//                // Sleep for a short duration before adding the next task
+//                try {
+//                    Thread.sleep(1000); // Sleep for 1 second
+//                } catch (InterruptedException e) {
+//                    Thread.currentThread().interrupt();
+//                }
+//            }
+//
+//            // Sleep for 5 seconds after adding all tasks to simulate no tasks being added
+//            try {
+//                Thread.sleep(5000); // Sleep for 5 seconds (no tasks added)
+//            } catch (InterruptedException e) {
+//                Thread.currentThread().interrupt();
+//            }
+//        });
+//
+//        taskAdderThread.start();
+//    }
 
 
 }
