c3ed998963e17293ad1fd9b489d8094a504459da
Natalie Kratzsch <nataliekratzsch@gmx.de>
2023-12-29 10:04:36 +0100

    ViewFactory ready

--- src/main/java/haw/vs/view/api/ViewFactory.java
+++ src/main/java/haw/vs/view/api/ViewFactory.java
@@ -2,14 +2,10 @@ package haw.vs.view.api;
 
 public class ViewFactory {
     public static IViewFacade getView(){
-        //TODO
         return new ViewFacade();
     }
     public static IPlayerInputHandler getInputHandler(){
-        //TODO
-        return (IPlayerInputHandler) new PlayerInputHandler();
-        //??? eigentlich klappt im lokalen Projekt
-        // return new PlayerInputHandler();
+        return new PlayerInputHandler();
     }
 }
 
