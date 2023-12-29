a78bbe6f1aabf2ec7029f664438031d394259752
Natalie Kratzsch <nataliekratzsch@gmx.de>
2023-12-29 14:49:40 +0100

    changed TronView, first idea

--- src/main/java/haw/vs/view/javafx/TronView.java
+++ src/main/java/haw/vs/view/javafx/TronView.java
@@ -4,9 +4,19 @@ import edu.cads.bai5.vsp.tron.view.ITronView;
 
 import java.io.IOException;
 
-public abstract class TronView implements ITronView {
+public class TronView extends edu.cads.bai5.vsp.tron.view.TronView implements ITronView {
     //TODO Singleton Umsetzung korrekt?
-    public ITronView getInstance() throws IOException {
-        return new edu.cads.bai5.vsp.tron.view.TronView();
+    public static TronView tronView;
+//TODO in Klassendiagramm updaten Konstruktor:
+    private TronView() throws IOException {
+        //super(); ??
+        tronView = new TronView();
+    }
+
+    public static ITronView getInstance() throws IOException {
+        if(tronView == null){
+            tronView = new TronView();
+        }
+        return tronView;
     }
 }
