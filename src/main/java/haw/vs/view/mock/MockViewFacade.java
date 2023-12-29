5f7ce62463e46850ea73ee07a96e12da8efb50ee
Natalie Kratzsch <nataliekratzsch@gmx.de>
2023-12-29 12:55:06 +0100

    added MockViewFacade

---
+++ src/main/java/haw/vs/view/mock/MockViewFacade.java
@@ -0,0 +1,41 @@
+package haw.vs.view.mock;
+
+import haw.vs.common.GameState;
+import haw.vs.view.api.IViewFacade;
+
+public class MockViewFacade implements IViewFacade {
+    @Override
+    public void startGame(GameState gameState) {
+
+    }
+
+    @Override
+    public void update(GameState gameState) {
+
+    }
+
+    @Override
+    public void playerLost(GameState gameState) {
+
+    }
+
+    @Override
+    public void playerWon(GameState gameState) {
+
+    }
+
+    @Override
+    public void updatePlayerCountView(int playerCount, int targetPlayerCount) {
+
+    }
+
+    @Override
+    public void showMainMenu() {
+
+    }
+
+    @Override
+    public void setMatchId(long matchId) {
+
+    }
+}
