43dc5a930251aeb01c97cfc3bcf2bc42f0cacee5
Natalie Kratzsch <nataliekratzsch@gmx.de>
2023-12-29 12:56:16 +0100

    added Mocks to ViewFactory

--- src/main/java/haw/vs/view/api/ViewFactory.java
+++ src/main/java/haw/vs/view/api/ViewFactory.java
@@ -1,11 +1,16 @@
 package haw.vs.view.api;
 
+import haw.vs.view.mock.MockPlayerInputHandler;
+import haw.vs.view.mock.MockViewFacade;
+
 public class ViewFactory {
     public static IViewFacade getView(){
-        return new ViewFacade();
+        //return new ViewFacade();
+        return new MockViewFacade();
     }
     public static IPlayerInputHandler getInputHandler(){
-        return new PlayerInputHandler();
+        //return new PlayerInputHandler();
+        return new MockPlayerInputHandler();
     }
 }
 
