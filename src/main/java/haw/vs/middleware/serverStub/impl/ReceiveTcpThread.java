4bab7fbc15ff2b4a2466907b7814bf45d121f1b1
Rebecca <rebecca.kassaye@haw-hamburg.de>
2023-12-30 13:41:51 +0100

    add run-impl receiveTcpThread

--- src/main/java/haw/vs/middleware/serverStub/impl/ReceiveTcpThread.java
+++ src/main/java/haw/vs/middleware/serverStub/impl/ReceiveTcpThread.java
@@ -1,13 +1,51 @@
 package haw.vs.middleware.serverStub.impl;
 
-public class ReceiveTcpThread implements Runnable{
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Arrays;
+
+public class ReceiveTcpThread implements Runnable {
     private ReceiveQueue receiveQueue;
+    private static final int TCP_PORT = 60124;
+
     public ReceiveTcpThread() {
         this.receiveQueue = ReceiveQueue.getInstance();
     }
 
     @Override
     public void run() {
-        // TODO impl for receiving TCP
+        try (ServerSocket welcomeSocket = new ServerSocket(TCP_PORT)) {
+            while (true) {
+                Socket clientSocket = welcomeSocket.accept();
+
+                Thread clientHandler = new Thread(() -> dealWithClient(clientSocket));
+                clientHandler.start();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void dealWithClient(Socket clientSocket) {
+        try {
+            InputStream incoming = clientSocket.getInputStream();
+            byte[] buffi = new byte[1024];
+            int bytesRead;
+
+            while ((bytesRead = incoming.read(buffi)) != -1) {
+                receiveQueue.put(Arrays.copyOf(buffi, bytesRead));
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try{
+                clientSocket.close();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
