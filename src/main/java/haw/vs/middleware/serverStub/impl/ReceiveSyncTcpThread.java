940448052eeafc7b54a543e082075739a2cf7286
Rebecca <rebecca.kassaye@haw-hamburg.de>
2023-12-31 13:08:05 +0100

    add impl ReceiveSyncTcpThread

--- src/main/java/haw/vs/middleware/serverStub/impl/ReceiveSyncTcpThread.java
+++ src/main/java/haw/vs/middleware/serverStub/impl/ReceiveSyncTcpThread.java
@@ -2,14 +2,60 @@ package haw.vs.middleware.serverStub.impl;
 
 import haw.vs.middleware.serverStub.api.ICaller;
 
-public class ReceiveSyncTcpThread implements Runnable{
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Arrays;
+
+public class ReceiveSyncTcpThread implements Runnable {
+    private static final int TCP_SYNC_PORT = 60125;
     private ICaller caller;
+
     public ReceiveSyncTcpThread() {
         this.caller = Caller.getInstance();
     }
 
     @Override
     public void run() {
-        // TODO impl for receiving sync TCP
+        try (ServerSocket welcomeSocket = new ServerSocket(TCP_SYNC_PORT)) {
+            while (true) {
+                Socket syncSocket = welcomeSocket.accept();
+
+                Thread clientHandler = new Thread(() -> dealWithSync(syncSocket));
+                clientHandler.start();
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void dealWithSync(Socket syncSocket) {
+        try {
+            InputStream incoming = syncSocket.getInputStream();
+            byte[] buffi = new byte[1024];
+            int bytesRead;
+
+            while ((bytesRead = incoming.read(buffi)) != -1) {
+                caller.callSynchronously(Arrays.copyOf(buffi, bytesRead));
+            }
+
+            OutputStream outgoing = syncSocket.getOutputStream();
+            String ok = "OK";
+            outgoing.write(ok.getBytes());
+            // TODO how/where if error?
+            outgoing.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                syncSocket.close();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
     }
 }
