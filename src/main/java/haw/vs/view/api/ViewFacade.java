5cc6cd1b063a0871f1a5c45c54166ffcf9ff77ee
Natalie Kratzsch <nataliekratzsch@gmx.de>
2024-01-04 10:36:41 +0100

    added showMainMenu in ViewFacade

--- src/main/java/haw/vs/view/api/ViewFacade.java
+++ src/main/java/haw/vs/view/api/ViewFacade.java
@@ -2,6 +2,7 @@ package haw.vs.view.api;
 
 import haw.vs.common.GameState;
 import haw.vs.view.javafx.TronViewAdapter;
+import haw.vs.view.overlay.MainMenuNew;
 import haw.vs.view.overlay.PlayerCountViewNew;
 
 import static haw.vs.view.javafx.TronView.tronView;
@@ -45,7 +46,9 @@ public class ViewFacade implements IViewFacade {
 
     @Override
     public void showMainMenu() {
-
+        MainMenuNew mainMenu = new MainMenuNew("main", tronView);
+        tronView.registerOverlay("main", mainMenu);
+        tronViewAdapter.showOverlay("main");
     }
 
     @Override
