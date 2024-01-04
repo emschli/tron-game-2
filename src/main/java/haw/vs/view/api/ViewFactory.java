28a7e746a43dac65ad90594641f20fabeac02c8e
Natalie Kratzsch <nataliekratzsch@gmx.de>
2024-01-04 13:57:53 +0100

    changed to mock after testing

--- src/main/java/haw/vs/view/api/ViewFactory.java
+++ src/main/java/haw/vs/view/api/ViewFactory.java
@@ -1,13 +1,14 @@
 package haw.vs.view.api;
 
 import haw.vs.view.mock.MockPlayerInputHandler;
+import haw.vs.view.mock.MockViewFacade;
 
 import java.io.IOException;
 
 public class ViewFactory {
     public static IViewFacade getView() throws IOException {
-        return new ViewFacade();
-        //return new MockViewFacade();
+        //return new ViewFacade();
+        return new MockViewFacade();
     }
     public static IPlayerInputHandler getInputHandler(){
         //return new PlayerInputHandler();
