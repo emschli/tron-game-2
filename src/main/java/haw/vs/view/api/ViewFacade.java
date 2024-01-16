42da182cb225b480396d158ac7fb05450d9045bf
Natalie Kratzsch <nataliekratzsch@gmx.de>
2024-01-16 23:29:15 +0100

    commented ViewFacade

--- src/main/java/haw/vs/view/api/ViewFacade.java
+++ src/main/java/haw/vs/view/api/ViewFacade.java
@@ -8,6 +8,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+/**
+ * Offers methods to trigger different view appearences
+ */
 public class ViewFacade implements IViewFacade {
     private final IView tronViewAdapter;
 
