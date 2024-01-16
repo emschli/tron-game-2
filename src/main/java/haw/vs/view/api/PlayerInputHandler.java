5fd90fb59a98f6a9956797b36692b61a1cbefc66
Natalie Kratzsch <nataliekratzsch@gmx.de>
2024-01-16 23:12:21 +0100

    commented IPlayerInputHandler

--- src/main/java/haw/vs/view/api/PlayerInputHandler.java
+++ src/main/java/haw/vs/view/api/PlayerInputHandler.java
@@ -18,6 +18,11 @@ public class PlayerInputHandler implements IPlayerInputHandler {
         this.inputController = inputController;
     }
 
+    /**
+     * Sets the target number of players with whom the user wants to play and sets the actual players to 1 (the user theirself)
+     * The configdata is created and the inputController is informed about this action and the data is directed to it.
+     * @param numOfPlayers number of players with whom the user wants to play
+     */
     @Override
     public void onGameStart(int numOfPlayers) {
         PlayerConfigData configData = new PlayerConfigData(600, 600);
@@ -26,17 +31,29 @@ public class PlayerInputHandler implements IPlayerInputHandler {
         inputController.joinGameController(getPlayerId(), numOfPlayers, configData);
     }
 
+    /**
+     * Sends the pressedKey to the input controller to handle the game action (direction changes)
+     * @param pressedKey
+     */
     @Override
     public void onKeyPressed(String pressedKey) {
                 System.out.println("Hilfsausgabe im PlayerInputHandler. Pressed Key: "+ pressedKey );
         inputController.handleGameActionController(getPlayerId(), getMatchId() , getDirectionFromString(pressedKey));
     }
 
+    /**
+     * Deletes the player from the waitingroom for the match for which they registered
+     */
     @Override
     public void onCancel() {
         inputController.cancelWaitController(getPlayerId(), getMatchId(), getNoOfPlayers());
     }
 
+    /**
+     * Converts a string to get the direction
+     * @param string to be converted
+     * @return direction
+     */
     private Direction getDirectionFromString(String string) {
         Direction direction = null;
 
